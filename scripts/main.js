35caf149af9c53343d200453d6094417b7b291d6
kevin <94207816+KevinV0212@users.noreply.github.com>
2023-06-05 22:41:24 -0400

    more specific pseudocode

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,39 @@
 function getComputerChoice(){
-    console.log("connected")
+    // generate a random number 1-3
+
+    // assign each number to a choice
+    // 1 - return rock; 2 - return paper; 3 - return scissors
+
 }
 
 function playRound(playerSelection, computerSelection){
+    // compare the the choices
+    
+    // print out who wins the round
 
+    // return a value to communicate who wins or if a tie happened(string, number, boolean)
 }
 
 function game(){
+    let userWins = 0;
+    let computerWins = 0;
+    // for 5 rounds:
+    //  prompt user for input (string)
+    //  getComputerChoice
+    // set a result variable to 
+    
+    // record result of playRound
 
+    // have variable(s) track who wins each round
+
+    // decide who wins based on which variable is larger than the other
+    // then print the appropriate message in console.
+
+    // print out message for user winning
+
+    // print out message for computer winning
+
+    // print out message for a tie
 }
 
 game();
